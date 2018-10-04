cd14c3346f607235fbb84167c7aeb0d79dc1ea2c
Sebastian Krysmanski <noreply@manski.net>
2018-10-04 13:51:59 +0200

    Indentation

--- index.php
+++ index.php
@@ -15,26 +15,26 @@ $isHorz = (@$_GET['t'] == 'horz');
 </head>
 
 <body>
-<div id="outer">
-  <div id="inner">
-    <div id="alert-container"></div>
+  <div id="outer">
+    <div id="inner">
+      <div id="alert-container"></div>
 
-    I'm a <b><?php echo $isHorz ? 'horizontal' : 'vertical'; ?></b> separator.
-    Drag <a class="me" href="" title="Drag me!"><?php echo $isHorz ? '───────────' : ''; ?></a> to your bookmarks <em><?php echo $isHorz ? 'folders' : 'toolbar'; ?></em>.<br><br>
+      I'm a <b><?php echo $isHorz ? 'horizontal' : 'vertical'; ?></b> separator.
+      Drag <a class="me" href="" title="Drag me!"><?php echo $isHorz ? '───────────' : ''; ?></a> to your bookmarks <em><?php echo $isHorz ? 'folders' : 'toolbar'; ?></em>.<br><br>
 
-    For a <b><?php echo $isHorz ? 'vertical' : 'horizontal'; ?></b> separator, click <a href="<?php echo $isHorz ? 'index.php' : 'index.php?t=horz'; ?>">here</a>.
+      For a <b><?php echo $isHorz ? 'vertical' : 'horizontal'; ?></b> separator, click <a href="<?php echo $isHorz ? 'index.php' : 'index.php?t=horz'; ?>">here</a>.
 
-    <br><br><b>:)</b><br><br><br>
+      <br><br><b>:)</b><br><br><br>
 
-    <p>
-      Made by <a href="https://www.twitter.com/manski83" target="_blank">@manski83</a>
-      (on <a href="https://github.com/skrysmanski/chrome-separators" target="_blank">GitHub</a>),<br>
-      based on work by <a href="https://www.twitter.com/VorteXLP" target="_blank">@VorteXLP</a>
-      <br/><br/>
-      <a href="https://twitter.com/share" class="twitter-share-button" data-text="Do you want separators in your Chrome bookmarks? Check this out! #tweettip" data-count="horizontal" data-via="manski83" data-url="https://separator.mayastudios.com/">Tweet</a><script type="text/javascript" src="https://platform.twitter.com/widgets.js"></script>
-    </p>
+      <p>
+        Made by <a href="https://www.twitter.com/manski83" target="_blank">@manski83</a>
+        (on <a href="https://github.com/skrysmanski/chrome-separators" target="_blank">GitHub</a>),<br>
+        based on work by <a href="https://www.twitter.com/VorteXLP" target="_blank">@VorteXLP</a>
+        <br/><br/>
+        <a href="https://twitter.com/share" class="twitter-share-button" data-text="Do you want separators in your Chrome bookmarks? Check this out! #tweettip" data-count="horizontal" data-via="manski83" data-url="https://separator.mayastudios.com/">Tweet</a><script type="text/javascript" src="https://platform.twitter.com/widgets.js"></script>
+      </p>
+    </div>
   </div>
-</div>
 
   <script>
     const detectedBrowser = browserDetect();
