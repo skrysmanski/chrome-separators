582dc21dc05180337db2f70bf9966311f6a05469
Sebastian Krysmanski <noreply@manski.net>
2018-10-04 11:44:13 +0200

    Added link to GitHub

--- index.php
+++ index.php
@@ -103,7 +103,8 @@ $isHorz = (@$_GET['t'] == 'horz');
     <br><br><b>:)</b><br><br><br>
 
     <p>
-      Made by <a href="https://www.twitter.com/manski83" target="_blank">@manski83</a>,<br>
+      Made by <a href="https://www.twitter.com/manski83" target="_blank">@manski83</a>
+      (on <a href="https://github.com/skrysmanski/chrome-separators" target="_blank">GitHub</a>),<br>
       based on work by <a href="https://www.twitter.com/VorteXLP" target="_blank">@VorteXLP</a><br>
       <a href="https://twitter.com/share" class="twitter-share-button" data-text="Do you want separators in your Chrome bookmarks? Check this out! #tweettip" data-count="horizontal" data-via="manski83" data-url="https://separator.mayastudios.com/">Tweet</a><script type="text/javascript" src="https://platform.twitter.com/widgets.js"></script>
     </p>
