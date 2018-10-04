cde85d0c09bbe434ebfc03797c131a798d3ebb20
Sebastian Krysmanski <skrysmanski@users.noreply.github.com>
2018-10-04 13:35:01 +0200

    Added browser detection for bug in Chrome 69

--- index.php
+++ index.php
@@ -4,97 +4,21 @@ $isHorz = (@$_GET['t'] == 'horz');
 ?>
 <html>
 <head>
-    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-    <title>Chrome Bookmarks Separator</title>
-    <link rel="icon" href="<?php echo $isHorz ? 'horz' : 'vert'; ?>-favicon.ico" type="image/x-icon">
-    <style>
-        html,
-        body {
-            width:100%;
-            height:100%;
-            margin:0px;
-            padding:0px;
-            font-family:Helvetica, Arial;
-        }
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+  <title>Chrome Bookmarks Separator</title>
+  <link rel="icon" href="<?php echo $isHorz ? 'horz' : 'vert'; ?>-favicon.ico" type="image/x-icon">
 
-        body {
-            background:#ececec;
-        }
+  <link rel="stylesheet" href="index.css" />
 
-        #outer {
-            position: absolute;
-            top: 50%;
-            left: 0px;
-            width: 100%;
-            height: 1px;
-            overflow: visible;
-        }
-
-        #inner {
-            width: 500px;
-            height: 480px;
-            margin-left: -250px;  /***  width / 2   ***/
-            position: absolute;
-            display:block;
-            vertical-align:middle;
-            font-size:29px;
-            color:#535559;
-            text-align:center;
-            text-shadow:0px 2px 1px #fff;
-            top: -240px;          /***  height / 2   ***/
-            left: 50%;
-        }
-
-        #inner a.me,
-        #inner a.me:active,
-        #inner a.me:visited,
-        #inner a.me:hover {
-            display:inline-block;
-            width:60px;
-            height:28px;
-
-            font-size: 0;
-            text-shadow: none;
-            color: #294995;
-
-            background: url("me.png") no-repeat;
-            background-size: 60px 28px;
-
-            vertical-align:middle;
-            text-decoration:none;
-        }
-
-        #inner b, #inner a {
-            font-weight:bold;
-            color:#335fa1;
-        }
-
-        p {
-            color:#a4a4a4;
-            font-size:13px;
-        }
-
-        p a {
-          text-decoration:none;
-        }
-
-        .twitter-share-button {
-            opacity:0.5;
-            margin-left:9px;
-            margin-top:5px;
-        }
-
-        .twitter-share-button:hover {
-            opacity:1;
-        }
-
-    </style>
-
+  <script src="node_modules/jquery/dist/jquery.min.js"></script>
+  <script src="node_modules/browser-detect/dist/browser-detect.umd.js"></script>
 </head>
 
 <body>
 <div id="outer">
   <div id="inner">
+    <div id="alert-container"></div>
+
     I'm a <b><?php echo $isHorz ? 'horizontal' : 'vertical'; ?></b> separator.
     Drag <a class="me" href="" title="Drag me!"><?php echo $isHorz ? '───────────' : ''; ?></a> to your bookmarks <em><?php echo $isHorz ? 'folders' : 'toolbar'; ?></em>.<br><br>
 
@@ -111,6 +35,19 @@ $isHorz = (@$_GET['t'] == 'horz');
     </p>
   </div>
 </div>
+
+  <script>
+    const detectedBrowser = browserDetect();
+
+    // Chrome 69 bug
+    if (detectedBrowser.name === "chrome" && detectedBrowser.versionNumber >= 69 && detectedBrowser.versionNumber < 70) {
+      $('#alert-container').html(`
+<div class="alert alert-warning" role="alert">
+  Unfortunately, due to <a href="https://productforums.google.com/forum/#!topic/chrome/sHG5ntLrdwQ" target="_blank">a bug in Chrome 69</a> you can't drag the "me" button to your bookmarks toolbar.
+</div>`);
+    }
+
+  </script>
 </body>
 
 </html>
