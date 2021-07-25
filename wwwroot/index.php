de123039fe9a39e0b0e6ccfd5629708cd6ffa5f5
Sebastian Krysmanski <noreply@manski.net>
2021-07-25 12:02:11 +0200

    Improved styling a little bit

--- wwwroot/index.php
+++ wwwroot/index.php
@@ -19,22 +19,32 @@ $isHorz = (@$_GET['t'] == 'horz');
 <body>
   <div id="outer">
     <div id="inner">
-      <div id="alert-container"></div>
-
-      I'm a <b><?php echo $isHorz ? 'horizontal' : 'vertical'; ?></b> separator.
-      Drag <a class="me" href="" title="Drag me!"><?php echo $isHorz ? '───────────' : ''; ?></a> to your bookmarks <em><?php echo $isHorz ? 'folders' : 'toolbar'; ?></em>.<br><br>
-
-      For a <b><?php echo $isHorz ? 'vertical' : 'horizontal'; ?></b> separator, click <a href="<?php echo $isHorz ? 'index.php' : 'index.php?t=horz'; ?>">here</a>.
-
-      <br><br><b>:)</b><br><br><br>
 
       <p>
-        Version <?php echo $VERSION; ?> made by <a href="https://www.twitter.com/manski83" target="_blank">@manski83</a>
-        (on <a href="https://github.com/skrysmanski/chrome-separators" target="_blank">GitHub</a>),<br>
-        based on work by <a href="https://www.twitter.com/VorteXLP" target="_blank">@VorteXLP</a>
-        <br/><br/>
-        <a href="https://twitter.com/share" class="twitter-share-button" data-text="Do you want separators in your Chrome bookmarks? Check this out! #tweettip" data-count="horizontal" data-via="manski83" data-url="https://separator.mayastudios.com/">Tweet</a><script type="text/javascript" src="https://platform.twitter.com/widgets.js"></script>
+        I'm a <b><?php echo $isHorz ? 'horizontal' : 'vertical'; ?></b> separator.
+        Drag <a class="me" href="" title="Drag me!"><?php echo $isHorz ? '───────────' : ''; ?></a><br/>
+        to your bookmarks <span class="target-name"><?php echo $isHorz ? 'folders' : 'toolbar'; ?></span>.
       </p>
+
+      <p>
+        For a <b><?php echo $isHorz ? 'vertical' : 'horizontal'; ?></b> separator, click <a href="<?php echo $isHorz ? 'index.php' : 'index.php?t=horz'; ?>">here</a>.
+      </p>
+
+      <p>
+        <b>:)</b>
+      </p>
+
+      <div class="footer">
+        <p>
+          Version <?php echo $VERSION; ?> made by <a href="https://www.twitter.com/manski83" target="_blank">@manski83</a>
+          (on <a href="https://github.com/skrysmanski/chrome-separators" target="_blank">GitHub</a>),<br>
+          based on work by <a href="https://www.twitter.com/VorteXLP" target="_blank">@VorteXLP</a>
+        </p>
+        <p>
+          <a href="https://twitter.com/share" class="twitter-share-button" data-text="Do you want separators in your Chrome bookmarks? Check this out! #tweettip" data-count="horizontal" data-via="manski83" data-url="https://separator.mayastudios.com/">Tweet</a><script type="text/javascript" src="https://platform.twitter.com/widgets.js"></script>
+        </p>
+        </p>
+      </div>
     </div>
   </div>
 </body>
