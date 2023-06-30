ea2c80b5b08cfdef45e641fc1eaf1f4756568d94
Kamikaze <timo1322@gmail.com>
2023-06-30 17:38:26 +0200

    Added random-hashtag function to bookmark button

--- wwwroot/index.php
+++ wwwroot/index.php
@@ -1,6 +1,6 @@
 <!doctype html>
 <?php
-$VERSION = '1.1';
+$VERSION = '1.2';
 $isHorz = (@$_GET['t'] == 'horz');
 ?>
 <html lang="en">
@@ -18,9 +18,24 @@ $isHorz = (@$_GET['t'] == 'horz');
   <div id="outer">
     <div id="inner">
 
+      <?php
+      function generateRandomHashtag($length = 10): string {
+        $characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+        $hashtag = '#';
+
+        for ($i = 0; $i < $length; $i++) {
+          $hashtag .= $characters[rand(0, strlen($characters) - 1)];
+        }
+
+        return $hashtag;
+      }
+
+      $randomHashtag = generateRandomHashtag();
+      ?>
+
       <p>
         I'm a <b><?php echo $isHorz ? 'horizontal' : 'vertical'; ?></b> separator.
-        Drag <a class="me" href="" title="Drag me!"><?php echo $isHorz ? '───────────' : ''; ?></a><br/>
+        Drag <a class="me" href="/<?php echo $randomHashtag; ?>" title="Drag me!"><?php echo $isHorz ? '───────────' : ''; ?></a><br/>
         to your bookmarks <span class="target-name"><?php echo $isHorz ? 'folders' : 'toolbar'; ?></span>.
       </p>
 
